e1b7ac1ed43a748a8fd451ec3a2350c4b06993b1
vikash verma <vikashraj144@gmail.com>
2016-02-16 17:41:23 +0530

    file updated
    file updated

---
+++ js/customs_main.js
@@ -0,0 +1,67 @@
+/* -- page 1 slider -- */
+
+$(document).ready(function(){
+	$('#full-page_slider').smSlider({
+		pagination : false,
+		subMenu    : true,
+		flexible   : true
+	});
+	$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+},  5000);
+
+
+
+$('#full-page_slider').smSlider({
+		pagination : false,
+		subMenu    : true,
+		flexible   : true
+	});
+	$("#slideshow2 > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow2 > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow2');
+},  5000);
+
+});
+
+
+
+
+$(window).load(function(){
+	setTimeout(function() {
+		$('#loader').css({'display':'none'});
+	}, 2000);
+	
+	$("#slideshow > div:gt(0)").hide();
+	
+	setInterval(function() { 
+		$('#slideshow > div:first')
+		.fadeOut(1000)
+		.next()
+		.fadeIn(1000)
+		.end()
+		.appendTo('#slideshow');
+		},  5000);
+	});
+
+
+
+
+$('.bxslider').bxSlider({
+	auto: true,
+	mode: 'fade',
+	pagerCustom: '#bx-pager'
+});
